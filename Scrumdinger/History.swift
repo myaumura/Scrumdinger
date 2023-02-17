ff671429a88ab438bc7b203746099352ce7b0fb3
Kirill Gusev <k3dosh@gmail.com>
2023-02-17 10:34:51 +0300

    Initial Commit

---
+++ Scrumdinger/History.swift
@@ -0,0 +1,24 @@
+//
+//  History.swift
+//  Scrumdinger
+//
+//  Created by Kirill Gusev on 26.01.2023.
+//
+
+import Foundation
+
+struct History: Identifiable, Codable {
+    let id: UUID
+    let date: Date
+    var attendees: [DailyScrum.Attendee]
+    var lengthInMinutes: Int
+    let transcript: String?
+    
+    init(id: UUID = UUID(), date: Date = Date(), attendees: [DailyScrum.Attendee], lengthInMinutes: Int = 5, transcript: String? = nil){
+        self.id = id
+        self.date = date
+        self.attendees = attendees
+        self.lengthInMinutes = lengthInMinutes
+        self.transcript = transcript
+    }
+}
